3bdac6cdedc091fe11b05fa78ab0b064f8688495
kbchoe <166102815+kbchoe@users.noreply.github.com>
2024-08-02 14:44:32 +0900

    break : indent to spaces
    too many changes!

--- linebody.py
+++ linebody.py
@@ -10,10 +10,10 @@ from math import dist
 
 #150E-3= 0.00150 , 150mm=0.0015m
 node_data = {
-	1:(0     ,0,0),
-	2:(150E-3,0,0),
-	3:(300E-3,0,0),
-	4:(600E-3,0,0),
+    1:(0     ,0,0),
+    2:(150E-3,0,0),
+    3:(300E-3,0,0),
+    4:(600E-3,0,0),
 }
 
 #250E-6 = 0.00 000 250/0.0000025,, 0 appears 6 times.
@@ -40,52 +40,52 @@ element_data = [
 #==========================================
 
 def lsm_1dSpring(K):
-	return k*np.array( (1,-1,-1,1) ).reshape(2,2)
+    return k*np.array( (1,-1,-1,1) ).reshape(2,2)
 
 def lsm_1dRod(E,A, p1,p2):
-	L = dist(p1,p2)
-	k = A*E/L
-	return k*np.array( (1,-1,-1,1) ).reshape(2,2)
+    L = dist(p1,p2)
+    k = A*E/L
+    return k*np.array( (1,-1,-1,1) ).reshape(2,2)
 
 def lsm_2dRod(E,A, p1,p2):
-	L = dist(p1,p2)	
-	k = A*E/L
-	#=============
-	# T = global->local disp transform matrix.
-	# C,S, 0,0 {u1}
-	#-S,C, 0,0 {v1}
-	# 0,0, C,S {u2}
-	# 0,0,-S,C {v2}
-	
-	# K = local stiffness matrix
-	# k,0,-k,0
-	# 0,0, 0,0
-	#-k,0, k,S
-	# 0,0, 0,C
-	
-	#and K_global = T.T@K@T
+    L = dist(p1,p2) 
+    k = A*E/L
+    #=============
+    # T = global->local disp transform matrix.
+    # C,S, 0,0 {u1}
+    #-S,C, 0,0 {v1}
+    # 0,0, C,S {u2}
+    # 0,0,-S,C {v2}
+    
+    # K = local stiffness matrix
+    # k,0,-k,0
+    # 0,0, 0,0
+    #-k,0, k,S
+    # 0,0, 0,C
+    
+    #and K_global = T.T@K@T
 
-	# theta = 0
-	# C = cos(theta)
-	# S = sin(theta)
-	x1,y1,_ = p1
-	x2,y2,_ = p2
-	C = (x2-x1)/L
-	S = (y2-y1)/L
-	C2 = C**2
-	S2 = S**2
-	CS = C*S
+    # theta = 0
+    # C = cos(theta)
+    # S = sin(theta)
+    x1,y1,_ = p1
+    x2,y2,_ = p2
+    C = (x2-x1)/L
+    S = (y2-y1)/L
+    C2 = C**2
+    S2 = S**2
+    CS = C*S
 
-	raw=(
-	 	 C2,  CS, -C2, -CS,
-		
-		 CS,  S2, -CS, -S2,
-		
-		-C2, -CS,  C2,  CS,
-		
-		-CS, -S2,  CS,  S2,
-	)
-	return k*np.array(raw).reshape(4,4)
+    raw=(
+         C2,  CS, -C2, -CS,
+        
+         CS,  S2, -CS, -S2,
+        
+        -C2, -CS,  C2,  CS,
+        
+        -CS, -S2,  CS,  S2,
+    )
+    return k*np.array(raw).reshape(4,4)
 
 
 
@@ -101,25 +101,25 @@ row_size = len(node_data)*node_dof
 
 GSM = np.zeros((row_size,row_size))
 for edata in element_data:
-	E = edata['E']
-	A = edata['A']	
-	
-	n1,n2 = edata['nodes']
-	p1 = node_data[n1]
-	p2 = node_data[n2]
-	
-	lsm = lsm_2dRod(E,A, p1,p2)
-	
-	nodes = edata['nodes']
+    E = edata['E']
+    A = edata['A']  
+    
+    n1,n2 = edata['nodes']
+    p1 = node_data[n1]
+    p2 = node_data[n2]
+    
+    lsm = lsm_2dRod(E,A, p1,p2)
+    
+    nodes = edata['nodes']
 
-	idx = []
-	for node in nodes:
-		#get indices by node_dof. dof3, node1=> 0,1,2.
-		x = [(node-1)*node_dof+i for i in range(node_dof)]
-		idx.extend(x)
+    idx = []
+    for node in nodes:
+        #get indices by node_dof. dof3, node1=> 0,1,2.
+        x = [(node-1)*node_dof+i for i in range(node_dof)]
+        idx.extend(x)
 
-	idx2d = np.ix_(idx,idx)  #(1,3)-> (1,1),(1,3),(3,1),(3,3)		
-	GSM[idx2d] += lsm
+    idx2d = np.ix_(idx,idx)  #(1,3)-> (1,1),(1,3),(3,1),(3,3)       
+    GSM[idx2d] += lsm
 
 
 
@@ -166,7 +166,7 @@ U_full[idx] = U
 #5.post-process
 #=====================
 for idx,i in enumerate(U_full):
-	print( f"node {idx+1} displacement: {i}")
+    print( f"node {idx+1} displacement: {i}")
 
 # node 1 displacement: 0.0
 # node 2 displacement: 0.0
@@ -192,25 +192,25 @@ for idx,i in enumerate(U_full):
 
 #to make dist
 # def dist(p1,p2):
-# 	# x1,y1,z1 = 0,0,0
-# 	# x2,y2,z2 = 0,0,0
-# 	# x1,y1,z1 = p1
-# 	# x2,y2,z2 = p2
-# 	# return ( (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2 )**0.5
-# 	return math.sqrt(sum((px - qx) ** 2.0 for px, qx in zip(p, q)))
+#   # x1,y1,z1 = 0,0,0
+#   # x2,y2,z2 = 0,0,0
+#   # x1,y1,z1 = p1
+#   # x2,y2,z2 = p2
+#   # return ( (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2 )**0.5
+#   return math.sqrt(sum((px - qx) ** 2.0 for px, qx in zip(p, q)))
 
 
 #more generalized data
 
 # force_data = {2:300E3}
 # for node in force_data:
-# 	F_full[node-1] = force_data[node]
+#   F_full[node-1] = force_data[node]
 
 # fixed_data = {1:(1,0,0),4:(1,0,0)}
 # def get_free_idx(fixed_data):
-# 	free_idx = []
-# 	for node,disp in fixed_data.items():
-# 		x = [ (node-1)*node_dof+i for i in range(node_dof) if disp[i]==0 ]
-# 		free_idx.extend(x)
-# 	return free_idx
+#   free_idx = []
+#   for node,disp in fixed_data.items():
+#       x = [ (node-1)*node_dof+i for i in range(node_dof) if disp[i]==0 ]
+#       free_idx.extend(x)
+#   return free_idx
 
