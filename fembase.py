87480d1c4b912129389b176e2c2a2d2446599072
kbchoe <166102815+kbchoe@users.noreply.github.com>
2024-08-08 15:52:33 +0900

    Q4 ATTACHED
    done!
    
    ansys not easyly allows
    add force to the
    mesh's node.
    (but edge.)
    so F value(N) not reproducible.

--- fembase.py
+++ fembase.py
@@ -43,6 +43,8 @@ def get_dof_data(etype):
         dof_data = ('x','y','rz')
     elif etype == '3dFrame':#xyz phi xyz        
         dof_data = ('x','y','z', 'rx','ry','rz')
+    elif etype == '2dQ4':
+        dof_data = ('x','y')
     return dof_data
 
 
@@ -239,6 +241,73 @@ def get_lsm_frame3d(E,A,Iy,Iz,G,J, p1,p2):
 
 
 
+def get_lsm_q4(E, nu, t, coords):
+    """
+    Computes the stiffness matrix for a Q4 element.
+
+    Parameters:
+    E (float): Young's modulus.
+    nu (float): Poisson's ratio. greek v 'new',
+    t (float): Thickness of the element.
+    coords (np.array): Nodal coordinates array of shape (4, 2).
+
+    Returns:
+    K (np.array): Stiffness matrix of shape (8, 8).
+    """
+    # Elasticity matrix for plane stress
+    C = E / (1 - nu ** 2) * np.array([
+        [1, nu, 0],
+        [nu, 1, 0],
+        [0, 0, (1 - nu) / 2]
+    ])
+    
+    coords = np.array(coords)[:,:2]#convinient list to -1,2 shaped.
+
+    # Gauss points and weights for 2x2 quadrature
+    gp = np.array([[-1, -1], [1, -1], [1, 1], [-1, 1]]) / np.sqrt(3)
+    weights = np.array([1, 1, 1, 1])
+
+    K = np.zeros((8, 8))
+
+    for i in range(len(gp)):
+        xi, eta = gp[i]
+        weight = weights[i]
+
+        # Shape functions and derivatives in natural coordinates
+        N = 0.25 * np.array([
+            (1 - xi) * (1 - eta),
+            (1 + xi) * (1 - eta),
+            (1 + xi) * (1 + eta),
+            (1 - xi) * (1 + eta)
+        ])
+
+        dN_dxi = 0.25 * np.array([
+            [-(1 - eta), -(1 - xi)],
+            [(1 - eta), -(1 + xi)],
+            [(1 + eta), (1 + xi)],
+            [-(1 + eta), (1 - xi)]
+        ])
+
+        # Jacobian matrix
+        J = dN_dxi.T @ coords
+        detJ = np.linalg.det(J)
+        invJ = np.linalg.inv(J)
+
+        # Derivatives of shape functions in physical coordinates
+        dN_dx = invJ @ dN_dxi.T
+
+        # Strain-displacement matrix B
+        B = np.zeros((3, 8))
+        for j in range(4):
+            B[0, j*2] = dN_dx[0, j]
+            B[1, j*2+1] = dN_dx[1, j]
+            B[2, j*2] = dN_dx[1, j]
+            B[2, j*2+1] = dN_dx[0, j]
+
+        # Element stiffness matrix
+        K += B.T @ C @ B * detJ * weight * t
+
+    return K
 
 
 
@@ -302,6 +371,12 @@ def get_lsm(edata, node_coords):
         G = edata['G']
         J = edata['J']
         lsm = get_lsm_frame3d(E,A,Iy,Iz,G,J, p1,p2)
+    
+    elif etype == '2dQ4':
+        E = edata['E']
+        nu = edata['nu']
+        t = edata['t']
+        lsm = get_lsm_q4(E,nu,t,node_coords)
 
     return lsm
 
@@ -368,7 +443,6 @@ def get_gsm(node_dof, node_data, element_data,elements):
 def get_load(node_dof, row_size, load_data):
     "is moment load?"
     F_full = np.zeros(row_size)
-
     for node_id,fxfyfz in load_data.items():
         idxs = nid_to_idxs(node_dof, node_id)
         for i,idx in enumerate(idxs):
@@ -408,10 +482,10 @@ def solve(mesh_data, load_data,fixed_data):
     etype = element_data[elements[0]['edata']]['etype']
     dof_data = get_dof_data(etype)
     node_dof = len(dof_data)
-    
-    
+
     
     GSM = get_gsm(node_dof, node_data, element_data,elements)
+    print(GSM.shape,node_dof,len(node_data))
     row_size = GSM.shape[0]
 
     F_full = get_load(node_dof, row_size, load_data)    
@@ -482,6 +556,31 @@ def post_process(mesh_data, load_data,fixed_data, solved_data):
     return post_data
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #=========================
 
 
@@ -529,6 +628,11 @@ fixed_data = {
 solve_data = solve( mesh_data,load_data,fixed_data)
 post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
 plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
+
+
+
+
+
 
 
 
@@ -586,6 +690,8 @@ plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
 
 
 
+
+
 node_data = {
     1:(0    ,0     ,0),
     2:(3.6  ,0     ,0),
@@ -630,6 +736,9 @@ fixed_data = {
 solve_data = solve( mesh_data,load_data,fixed_data)
 post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
 plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
+
+
+
 
 
 
@@ -692,6 +801,13 @@ fixed_data = {
 solve_data = solve( mesh_data,load_data,fixed_data)
 post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
 plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
+
+
+
+
+
+
+
 
 
 
@@ -759,3 +875,185 @@ post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
 plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
 plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}',scale=50,true_ratio=True)
 
+
+
+
+
+
+
+
+node_data = {
+    1:(3,2,0),
+    2:(5,2,0),
+    3:(5,4,0),
+    4:(3,4,0),
+    5:(7,2,0),
+    6:(7,4,0),    
+}
+
+element_data = {
+    1:{'E':30e6,'nu':0.25,'t':1, 'etype':'2dQ4'},
+}
+elements = [
+{ 'edata':1,'nodes':(1,2,3,4) },
+{ 'edata':1,'nodes':(2,5,6,3) },
+]
+
+mesh_data = {
+    'node_data':node_data,
+    'element_data':element_data,
+    'elements':elements,
+}
+
+load_data = {
+    5: (0,10000,0),
+}
+fixed_data = {
+1:(1,1,1),
+4:(1,1,1),
+}
+
+solve_data = solve( mesh_data,load_data,fixed_data)
+post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
+plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
+
+
+
+
+
+
+
+
+
+node_data = {
+        1 : (   2.665846445E-02,    -2.429309196E-02     ,0.000000000E+00),
+        2 : (   1.910056381E-02,    -2.781068473E-02     ,0.000000000E+00),
+        3 : (   2.757787033E-02,    -3.285615412E-02     ,0.000000000E+00),
+        4 : (   1.892646128E-02,    -3.412359215E-02     ,0.000000000E+00),
+        5 : (   3.685549030E-02,    -3.162841396E-02     ,0.000000000E+00),
+        6 : (   3.625732252E-02,    -2.204547664E-02     ,0.000000000E+00),
+        7 : (   4.605848486E-02,    -2.091198696E-02     ,0.000000000E+00),
+        8 : (   4.638304947E-02,    -3.083040555E-02     ,0.000000000E+00),
+        9 : (   5.576492586E-02,    -2.036832789E-02     ,0.000000000E+00),
+       10: (    5.595039144E-02,    -3.038448976E-02     ,0.000000000E+00),
+       11: (    6.548125261E-02,    -3.012011515E-02     ,0.000000000E+00),
+       12: (    6.539707048E-02,    -2.011817555E-02     ,0.000000000E+00),
+       13: (    5.617867522E-02,    -4.020133572E-02     ,0.000000000E+00),
+       14: (    6.562915990E-02,    -4.007241204E-02     ,0.000000000E+00),
+       15: (    4.670551188E-02,    -4.042668645E-02     ,0.000000000E+00),
+       16: (    3.726522046E-02,    -4.080290924E-02     ,0.000000000E+00),
+       17: (    2.794232309E-02,    -4.132705140E-02     ,0.000000000E+00),
+       18: (    1.880731545E-02,    -4.180840832E-02     ,0.000000000E+00),
+       19: (    1.011865872E-02,    -3.341606272E-02     ,0.000000000E+00),
+       20: (    9.596725580E-03,    -4.182446076E-02     ,0.000000000E+00),
+       21: (    1.194217511E-02,    -2.390264269E-02     ,0.000000000E+00),
+       22: (    2.402667543E-02,    -1.381114917E-02     ,0.000000000E+00),
+       23: (    3.576028442E-02,    -1.148416783E-02     ,0.000000000E+00),
+       24: (    4.595863570E-02,    -1.059936790E-02     ,0.000000000E+00),
+       25: (    5.573528261E-02,    -1.021622944E-02     ,0.000000000E+00),
+       26: (    6.538354656E-02,    -1.006023238E-02     ,0.000000000E+00),
+       27: (    7.500000000E-02,    -2.000000030E-02     ,0.000000000E+00),
+       28: (    7.500000000E-02,    -1.000000015E-02     ,0.000000000E+00),
+       29: (    7.500000000E-02,    -3.000000045E-02     ,0.000000000E+00),
+       30: (    7.500000000E-02,    -4.000000060E-02     ,0.000000000E+00),
+       31: (    1.604662371E-02,    -9.206294994E-03     ,0.000000000E+00),
+       32: (    9.341717358E-03,    -1.596816573E-02     ,0.000000000E+00),
+       33: (    6.587775262E-02,    -5.000000000E-02     ,0.000000000E+00),
+       34: (    7.500000298E-02,    -5.000000075E-02     ,0.000000000E+00),
+       35: (    5.646667724E-02,    -5.000000000E-02     ,0.000000000E+00),
+       36: (    4.705552687E-02,    -5.000000000E-02     ,0.000000000E+00),
+       37: (    3.764445150E-02,    -5.000000000E-02     ,0.000000000E+00),
+       38: (    2.823330112E-02,    -5.000000000E-02     ,0.000000000E+00),
+       39: (    1.882222575E-02,    -5.000000000E-02     ,0.000000000E+00),
+       40: (    9.411075374E-03,    -5.000000000E-02     ,0.000000000E+00),
+       41:(     2.734326767E-02,     0.000000000E+00     ,0.000000000E+00),
+       42:(     1.850000024E-02,     0.000000000E+00     ,0.000000000E+00),
+       43:(     3.680747013E-02,     0.000000000E+00     ,0.000000000E+00),
+       44:(     4.635557509E-02,     0.000000000E+00     ,0.000000000E+00),
+       45:(     5.590373655E-02,     0.000000000E+00     ,0.000000000E+00),
+       46:(     6.545184152E-02,     0.000000000E+00     ,0.000000000E+00),
+       47:(     7.500000298E-02,     0.000000000E+00     ,0.000000000E+00),
+       48:(    -8.411400452E-19,    -4.220340412E-02     ,0.000000000E+00),
+       49:(     0.000000000E+00,    -5.000000075E-02     ,0.000000000E+00),
+       50:(    -1.707526847E-18,    -3.417279399E-02     ,0.000000000E+00),
+       51:(    -2.557363465E-18,    -2.629558886E-02     ,0.000000000E+00),
+       52:(     0.000000000E+00,    -1.850000024E-02     ,0.000000000E+00),
+}
+
+element_data = {
+    1:{'E':200e9,'nu':0.25,'t':0.025, 'etype':'2dQ4'},
+}
+elements = [
+{'edata':1, 'nodes':  ( 1,2,4,3),},
+{'edata':1, 'nodes':  ( 3,5,6,1),},
+{'edata':1, 'nodes':  ( 7,6,5,8),},
+{'edata':1, 'nodes':  ( 9,7,8,10),},
+{'edata':1, 'nodes': (11,12,9,10),},
+{'edata':1, 'nodes': (13,14,11,10),},
+{'edata':1, 'nodes': (15,13,10,8),},
+{'edata':1, 'nodes': (16,15,8,5),},
+{'edata':1, 'nodes': (17,16,5,3),},
+{'edata':1, 'nodes': (18,17,3,4),},
+{'edata':1, 'nodes': (19,20,18,4),},
+{'edata':1, 'nodes': (21,19,4,2),},
+{'edata':1, 'nodes': (22,21,2,1),},
+{'edata':1, 'nodes': (23,22,1,6),},
+{'edata':1, 'nodes': (24,23,6,7),},
+{'edata':1, 'nodes': (25,24,7,9),},
+{'edata':1, 'nodes': (26,25,9,12),},
+{'edata':1, 'nodes': (27,28,26,12),},
+{'edata':1, 'nodes': (29,27,12,11),},
+{'edata':1, 'nodes': (30,29,11,14),},
+{'edata':1, 'nodes': (31,32,21,22),},
+{'edata':1, 'nodes': (33,34,30,14),},
+{'edata':1, 'nodes': (35,33,14,13),},
+{'edata':1, 'nodes': (36,35,13,15),},
+{'edata':1, 'nodes': (37,36,15,16),},
+{'edata':1, 'nodes': (38,37,16,17),},
+{'edata':1, 'nodes': (39,38,17,18),},
+{'edata':1, 'nodes': (40,39,18,20),},
+{'edata':1, 'nodes': (41,42,31,22),},
+{'edata':1, 'nodes': (43,41,22,23),},
+{'edata':1, 'nodes': (44,43,23,24),},
+{'edata':1, 'nodes': (45,44,24,25),},
+{'edata':1, 'nodes': (46,45,25,26),},
+{'edata':1, 'nodes': (47,46,26,28),},
+{'edata':1, 'nodes': (48,49,40,20),},
+{'edata':1, 'nodes': (50,48,20,19),},
+{'edata':1, 'nodes': (51,50,19,21),},
+{'edata':1, 'nodes': (52,51,21,32),},
+]
+mesh_data = {
+    'node_data':node_data,
+    'element_data':element_data,
+    'elements':elements,
+}
+# CMBLOCK,_FIXEDSU,NODE,        5
+# (8i10)
+#         48        49        50        51        52
+
+fixed_data = {
+48:(1,1,1),
+49:(1,1,1),
+50:(1,1,1),
+51:(1,1,1),
+52:(1,1,1),
+}
+f=100000
+load_data = {
+27:(f,0,0),
+28:(f,0,0),
+29:(f,0,0),
+30:(f,0,0),
+34:(f,0,0),
+47:(f,0,0),
+}
+       # 39        2        2        2       12       28       27
+       # 40        2        2        2       12       27       29
+       # 41        2        2        2       12       29       30
+       # 42        2        2        2       12       30       34 ->
+       # 43        2        2        2       12       47       28 ->(47,28 to a line)
+
+solve_data = solve( mesh_data,load_data,fixed_data)
+post_data = post_process( mesh_data,load_data,fixed_data, solve_data)
+plot_elements(post_data, title= f'3D Line plot for {element_data[1]['etype']}')
